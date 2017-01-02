d0372c30bec7d61e380f0a21cce3ff25a5012d6e
Josh Bivens <hotbioform@gmail.com>
2017-01-02 08:40:18 -0700

    Add gradients list

--- colorgen.js
+++ colorgen.js
@@ -1,3 +1,27 @@
 // Select our divs
 const div1 = document.querySelector("div1");
 const div2 = document.querySelector("div2");
+
+// Create list of hex values with starting and ending values
+const gradients = [
+  {
+    start: "#4ca2cd",
+    end: "#67B26F"
+  },
+  {
+    start: "#ee0979",
+    end: "#ff6a00"
+  },
+  {
+    start: "#F3904F",
+    end: "#3B4371"
+  },
+  {
+    start: "#3a6186",
+    end: "#89253e"
+  },
+  {
+    start: "#16BFFD",
+    end: "#CB3066"
+  },
+]
