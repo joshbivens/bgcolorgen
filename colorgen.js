9561443e7b346efef05c15c67327f6ee2bc0a91f
Josh Bivens <hotbioform@gmail.com>
2017-01-03 08:47:35 -0700

    Make cards/populate with color values

--- colorgen.js
+++ colorgen.js
@@ -1,42 +1,53 @@
 // Select our page's elements
+// -- Div 1
+const div1 = document.querySelector(".div1");
+const color1 = div1.children[0];
+
+// -- Div 2
+const div2 = document.querySelector(".div2");
+const color2 = div2.children[0];
+
+// Misc
 const container = document.querySelector(".container");
-const div1 = document.querySelector("#div1");
-const div2 = document.querySelector("#div2");
+const gradientInfo = document.querySelector(".gradient-info");
 const button = document.querySelector("button");
 
 // Create list of hex values with starting and ending values
 const gradients = [
   {
-    start: "#4ca2cd",
+    start: "#4CA2CD",
     end: "#67B26F"
   },
   {
-    start: "#ee0979",
-    end: "#ff6a00"
+    start: "#EE0979",
+    end: "#FF6A00"
   },
   {
     start: "#F3904F",
     end: "#3B4371"
   },
   {
-    start: "#3a6186",
-    end: "#89253e"
+    start: "#43C6AC",
+    end: "#F8FFAE"
   },
   {
     start: "#16BFFD",
     end: "#CB3066"
   },
+  {
+    start: "#FF5F6D",
+    end: "#FFC371"
+  },
+  {
+    start: "#004FF9",
+    end: "#FFF94C" 
+  }
 ];
 
 // So we want to pick one of the gradients at random
 const random = (xs) => xs[Math.floor(Math.random() * xs.length)];
 
-// Now we want to apply the new hex values to our bg and divs
-// container.style.background = `linear-gradient(to bottom, ${random(gradients).start}, ${random(gradients).end})`;
-// div1.style.background = `${random(gradients).start}`;
-// div2.style.background = `${random(gradients).end}`;
-
-// Now put em in an click event
+// Event Handlers
 button.addEventListener("click", handleClick);
 
 function handleClick() {
@@ -44,13 +55,15 @@ function handleClick() {
 }
 
 function setNewColors(arr) {
+  // Cache random new color
   let newColors = random(arr);
-  container.style.background = `linear-gradient(to bottom, ${newColors.start}, ${newColors.end})`;
+  // Use it values to populate a linear-gradient
+  let gradient = `linear-gradient(to right, ${newColors.start}, ${newColors.end})`;
+  // Set bg and text properties
+  container.style.background = gradient;
+  gradientInfo.innerHTML = gradient;
   div1.style.background = `${newColors.start}`;
+  color1.innerHTML = `${newColors.start}`
   div2.style.background = `${newColors.end}`;
+  color2.innerHTML = `${newColors.end}`
 }
-
-// TODO:
-// - Add inital color values (DONE)
-// - Add the text values of the colors to the color cards
-// - Add another box that displays the gradient info
